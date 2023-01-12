94944dc9aee1d4c196335f7a97d0a5aacc17a2c3
xlan11 <alankelly@gmail.com>
2023-01-12 22:55:08 +0000

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# the-movies
-
-Movie database showing top movies and upcoming movies using the TMDB api
--- the-movies/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return (
-        <footer><h1>footer</h1></footer>
-    )
-}
